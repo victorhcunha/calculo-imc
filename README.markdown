cb3e0f95b6992e0a70091c29e739bc1942e1ea48
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-06-04 19:58:14 -0300

    projeto

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calcule seu IMC
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/victorhcunha/pen/zYZRxZB](https://codepen.io/victorhcunha/pen/zYZRxZB).
+
+
---
+++ src/script.js
@@ -0,0 +1,34 @@
+let btnCalcular = document.getElementById("btnCalcular");
+
+function calcularIMC(){
+  let peso = document.getElementById("peso").value;
+  let altura = document.getElementById("altura").value;
+  let resultado = document.getElementById("resultado");
+  
+  if(altura != "" && peso !=""){
+    let imc = (peso / (altura * altura)).toFixed(2);
+    let mensagem = ""
+    
+    if(imc < 18.5){
+      mensagem = "Você está abaixo do peso"
+    }else if(imc < 25){
+      mensagem = "Você está com o peso ideal"
+    }else if(imc < 30){
+      mensagem = "Você está levemente acima do peso"
+    }else if(imc < 35){
+      mensagem = "Você está com obesidade grau I"
+    }else if(imc < 40){
+      mensagem = "Você está com obesidade grau II"
+    }else{
+      mensagem = "Você está com obesidade grau III"
+    }
+    resultado.textContent = `Seu IMC é ${imc}. ${mensagem}`;
+    
+  }else{
+    resultado.textContent = "Preencha todos os campos";
+  }
+  
+  
+}
+
+btnCalcular.addEventListener("click",calcularIMC);
